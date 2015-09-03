c3b1a7e3bd78c2671e3c92433e639543a6893110
Danilo Velasquez <dvelasquez@valentys.com>
2015-09-03 16:11:44 -0300

    Update System

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,12 +1,21 @@
 /**
  * Created by dvelur on 22-08-15.
  */
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var bower = require('main-bower-files');
-var inject = require('gulp-inject');
-var series = require('stream-series');
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    bower = require('main-bower-files'),
+    series = require('stream-series'),
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint'),
+    inject = require('gulp-inject'),
+    concat = require('gulp-concat'),
+    annotate = require('gulp-ng-annotate'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    runSequence = require('run-sequence'),
+    gutil = require('gulp-util');
 
+//DEV TASK
 gulp.task('copy-lib', function() {
     return gulp.src(bower(), { base: 'bower_components' })
         .pipe(gulp.dest('src/lib/'));
@@ -15,7 +24,7 @@ gulp.task('copy-lib', function() {
 gulp.task('index', function () {
     var vendorStream = gulp.src(['src/lib/**/*.js','src/lib/**/*.css'], {read: false});
 
-    var appStream = gulp.src(['src/index.js','src/app/**/*.js', 'src/resources/css/**/*.css'], {read: false});
+    var appStream = gulp.src(['src/index.js','src/app/**/*.js','src/app/**/*.css'], {read: false});
 
     gulp.src('src/index.html')
         .pipe(inject(series(vendorStream, appStream),{relative:true}))
@@ -40,3 +49,66 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['copy-lib','index','connect', 'watch']);
+
+//END DEV TASK
+
+//DIST TASK
+gulp.task('clean-build', function(){
+    return gulp.src('./dist')
+        .pipe(clean());
+});
+
+gulp.task('build-app-js', function () {
+    return gulp.src(['src/index.js','src/app/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(annotate())
+        .pipe(concat('app.min.js'))
+        .pipe(uglify().on('error', gutil.log))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-app-css', function () {
+    return gulp.src(['src/**/*.css', '!src/lib/**/*.*'])
+        .pipe(concat('app.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-vendor-js', function () {
+    return gulp.src(['src/lib/**/*.js'])
+        .pipe(jshint())
+        .pipe(annotate())
+        .pipe(concat('lib.min.js'))
+        .pipe(uglify().on('error', gutil.log))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-vendor-css', function () {
+    return gulp.src('src/lib/**/*.css')
+        .pipe(concat('lib.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-files', function () {
+    return gulp.src(['src/**/*.html', 'src/resources/**/*.*'])
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-inject', function () {
+    var target = gulp.src('./dist/index.html');
+
+    var appStream = gulp.src(['./dist/app.min.js', './dist/app.min.css']);
+
+    var vendorStream = gulp.src(['./dist/lib.min.js','./dist/lib.min.css']);
+
+    return target.pipe(inject(series(vendorStream, appStream), {ignorePath: 'dist/', addRootSlash: false}))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-dist', function(cb){
+    return runSequence('clean-build', 'build-app-js', 'build-app-css', 'build-vendor-js',
+        'build-vendor-css', 'copy-files', 'build-inject', cb);
+});
+//END DIST DASK
